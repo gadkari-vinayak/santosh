3c430513c112a60e10324087e1602ebaab6f12d5
gadkari-vinayak <gadkarivinayak0@gmail.com>
2019-07-09 16:35:14 +0530

    edit

--- cloudrun/app.py
+++ cloudrun/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    target = os.environ.get('TARGET', 'santosh')
+    target = os.environ.get('TARGET', 'World')
     return 'Hello {}!\n'.format(target)
 
 if __name__ == "__main__":
